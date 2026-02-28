f0daee0f39cb174f484136ad1c760282a45b2425
xXCheeseSliceXx <davidhenning03@gmail.com>
2026-02-28 11:25:26 +0100

    added testing MainMenu
    Added testing MainMenu
    Added PlayButton redirecting to scene CombatScreen
    Added MenuButton for Settings Popup
    Added QuitButton

---
+++ scripts/autoload/ContentDB.gd
@@ -0,0 +1,76 @@
+extends Node
+
+var spells: Dictionary = {}
+var enemies: Dictionary = {}
+
+func _ready() -> void:
+	_load_spells()
+	_load_enemies()
+
+	print("ContentDB loaded:")
+	print("Spells: ", spells.keys())
+	print("Enemies: ", enemies.keys())
+
+# LOADERS
+func _load_spells() -> void:
+	var path := "res://resources/spells/"
+	var dir := DirAccess.open(path)
+	if dir == null:
+		push_error("Failed to open spells directory")
+		return
+
+	dir.list_dir_begin()
+	var file_name := dir.get_next()
+
+	while file_name != "":
+		if file_name.ends_with(".tres"):
+			var full_path = path + file_name
+			var res = load(full_path)
+
+			if res != null and res.has_method("get"):
+				if res.id != "":
+					spells[res.id] = res
+				else:
+					push_warning("Spell missing id: " + full_path)
+
+		file_name = dir.get_next()
+
+	dir.list_dir_end()
+
+func _load_enemies() -> void:
+	var path := "res://resources/enemies/"
+	var dir := DirAccess.open(path)
+	if dir == null:
+		push_error("Failed to open enemies directory")
+		return
+
+	dir.list_dir_begin()
+	var file_name := dir.get_next()
+
+	while file_name != "":
+		if file_name.ends_with(".tres"):
+			var full_path = path + file_name
+			var res = load(full_path)
+
+			if res != null:
+				if res.id != "":
+					enemies[res.id] = res
+				else:
+					push_warning("Enemy missing id: " + full_path)
+
+		file_name = dir.get_next()
+
+	dir.list_dir_end()
+
+# GETTERS
+func get_spell(id: String):
+	return spells.get(id, null)
+
+func get_enemy(id: String):
+	return enemies.get(id, null)
+
+func get_all_spells() -> Array:
+	return spells.values()
+
+func get_all_enemies() -> Array:
+	return enemies.values()
--- scripts/ui/menu/main_menu.gd
+++ scripts/ui/menu/main_menu.gd
@@ -1,19 +1,19 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	print("Menu loaded")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-
-func _on_button_pressed() -> void:
 	pass # Replace with function body.
-
-
+	
+func _on_play_button_pressed() -> void:
+	print("Play pressed!") # <- test
+	get_tree().change_scene_to_file("res://scenes/game/combat/CombatScreen.tscn")
+	
 func _on_menu_button_pressed() -> void:
 	pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
