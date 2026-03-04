2e8499482e84659ba107c98c02c55f8d453f0ef0
xXCheeseSliceXx <163018665+xXCheeseSliceXx@users.noreply.github.com>
2026-03-04 15:21:26 +0100

    implemented test-aoe-spells

--- scripts/combat/spells/spell_type_resources/aoe_spell_resource.gd
+++ scripts/combat/spells/spell_type_resources/aoe_spell_resource.gd
@@ -1,12 +1,11 @@
-extends SpellResource
+extends ProjectileSpellResource
 class_name AOESpellResource
 
 enum AoEMode { ONCE, DOT }
 
 @export var aoe_mode: AoEMode
 @export var aoe_radius: float
-@export var expansion_speed: float
-@export var projectile_speed: float
+@export var aoe_delay: float = 0.0
 
 # For ONCE
 @export var aoe_damage_once: int
@@ -15,8 +14,6 @@ enum AoEMode { ONCE, DOT }
 @export var aoe_duration: float
 @export var aoe_tick_damage: int
 @export var aoe_tick_rate: float
-
-
 
 @export var aoe_frames: SpriteFrames
 @export var aoe_anim: StringName = &"default"
