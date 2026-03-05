bb9e505508117bec958b2284fbeebbf3a054dd57
xXCheeseSliceXx <davidhenning03@gmail.com>
2026-03-05 13:38:22 +0100

    Added SettingsWindow and MainMenu cleanup

--- scripts/ui/menu/main_menu.gd
+++ scripts/ui/menu/main_menu.gd
@@ -1,13 +1,18 @@
 extends Control
 
+@onready var settings_window: Window = $SettingsWindow
+
 func _ready() -> void:
-	pass
+	settings_window.hide()
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game/RunRoot.tscn")
-	
+
 func _on_menu_button_pressed() -> void:
-	pass
+	settings_window.popup_centered()
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_close_button_pressed() -> void:
+	settings_window.hide()
