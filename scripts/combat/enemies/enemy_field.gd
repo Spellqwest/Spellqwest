f30154b47c6d91a6d7f2104bd289e6340cf89ea1
CoderKyrial <Julian.Buttle@gmx.de>
2026-03-13 16:23:15 +0100

    Code adjustment
    - Changed from Dictionary to Array for enemy spawpoints (Yes, the dictionary was a bad practice)
    - Adjusted the empty array initialization to modern godot scripting.
    - Adjusted variable names

--- scripts/combat/enemies/enemy_field.gd
+++ scripts/combat/enemies/enemy_field.gd
@@ -7,30 +7,32 @@ extends Node2D
 
 var baseYPos = 100
 
-var enemy_waves: Dictionary = {
-	1: Vector2(314.0, baseYPos),
-	2: Vector2(446.0, baseYPos),
-	3: Vector2(574.0, baseYPos),
-	4: Vector2(698.0, baseYPos),
-	5: Vector2(826.0, baseYPos)
-}
+var enemy_spawnpoints: Array[Vector2] = [
+	Vector2(314.0, baseYPos),
+	Vector2(446.0, baseYPos),
+	Vector2(574.0, baseYPos),
+	Vector2(698.0, baseYPos),
+	Vector2(826.0, baseYPos)
+]
+var blocked_enemy_spawns: Array[int] = []
+
+var randomPos: int
 var rng = RandomNumberGenerator.new()
-var randomPos = -1
-var blocked_enemy_positions = Array([], TYPE_INT, "", null)
+
 
 ##############################################
 #Functions
 ##############################################
 func _on_enemy_spawn_timer_timeout() -> void:
 	var enemy = enemy_scene.instantiate()
-	randomPos = rng.randi_range(1, 5)
+	randomPos = rng.randi_range(0, 4)
 	
-	if(!blocked_enemy_positions.has(randomPos)):
-		enemy.position = enemy_waves[randomPos]
-		blocked_enemy_positions.append(randomPos)
-		print("used enemy positions: " + str(blocked_enemy_positions))
+	if(!blocked_enemy_spawns.has(randomPos)):
+		enemy.position = enemy_spawnpoints[randomPos]
+		blocked_enemy_spawns.append(randomPos)
+		print("used enemy positions: " + str(blocked_enemy_spawns))
 		print("spawning enemy on: " + str(enemy.position))
 		add_child(enemy)
 	print("in timer method mit rng: " + str(randomPos))
-	if(blocked_enemy_positions.size() == 5): 
-		blocked_enemy_positions = Array([], TYPE_INT, "", null)
+	if(blocked_enemy_spawns.size() == 5): 
+		blocked_enemy_spawns = Array([], TYPE_INT, "", null)
