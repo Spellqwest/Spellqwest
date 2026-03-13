45fcfea989b8e9214db8c5c4372e1cbd688664b4
CoderKyrial <Julian.Buttle@gmx.de>
2026-03-13 14:14:07 +0100

    First Enemy Model and Minor Changes
    - Added the gnome as current enemy sprite
    - Changed some variable names
    - Changed in enemy_field from a Dictionary to a normal array for spawnpoints

--- scenes/game/combat/entities/enemy_field.gd
+++ scenes/game/combat/entities/enemy_field.gd
@@ -5,28 +5,29 @@ extends Node2D
 ##############################################
 @export var enemy_scene:= preload("res://scenes/game/combat/entities/Enemy.tscn") 
 
-var baseYPos = 100
+const baseYPos = 100
 
-var enemy_waves: Dictionary = {
-	1: Vector2(314.0, baseYPos),
-	2: Vector2(446.0, baseYPos),
-	3: Vector2(574.0, baseYPos),
-	4: Vector2(698.0, baseYPos),
-	5: Vector2(826.0, baseYPos)
-}
+var enemy_spawnpoints: Array[Vector2] = [
+	Vector2(314.0, baseYPos),
+	Vector2(446.0, baseYPos),
+	Vector2(574.0, baseYPos),
+	Vector2(698.0, baseYPos),
+	Vector2(826.0, baseYPos)
+]
+
 var rng = RandomNumberGenerator.new()
-var randomPos = -1
-var blocked_enemy_positions = Array([], TYPE_INT, "", null)
+var randomPos: int
+var blocked_enemy_positions: Array[int] = []
 
 ##############################################
 #Functions
 ##############################################
 func _on_enemy_spawn_timer_timeout() -> void:
 	var enemy = enemy_scene.instantiate()
-	randomPos = rng.randi_range(1, 5)
+	randomPos = rng.randi_range(0, 4)
 	
 	if(!blocked_enemy_positions.has(randomPos)):
-		enemy.position = enemy_waves[randomPos]
+		enemy.position = enemy_spawnpoints[randomPos]
 		blocked_enemy_positions.append(randomPos)
 		print("used enemy positions: " + str(blocked_enemy_positions))
 		print("spawning enemy on: " + str(enemy.position))
